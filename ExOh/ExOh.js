439db136cd18f9e1cd2d3e8c2591cb31b645a2f8
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-07-17 05:42:05 +0600

    ExOh count problem solved

--- ExOh/ExOh.js
+++ ExOh/ExOh.js
@@ -4,3 +4,21 @@ string true if there is an equal number of x's and o's, otherwise return the str
 these two letters will be entered in the string, no punctuation or numbers. For example: if str is
 "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
  */
+
+const ExOh=(str)=>{
+    const Ex=/[x]/i
+    const Oh=/[o]/i
+    let ExCount=0
+    let OhCount=0;
+    for(let i=0;i<str.length;i++){
+        if(str[i].match(Ex)){
+            ExCount++
+        }
+        if(str[i].match(Oh)){
+            OhCount++
+        }
+    }
+    return ExCount===OhCount;
+}
+
+console.log(ExOh('xoxoxoooxx'))
